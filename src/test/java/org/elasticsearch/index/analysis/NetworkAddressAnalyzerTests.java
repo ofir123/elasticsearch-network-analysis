289a3d083a51d846b892dbc9f0a35745beb6f6f4
Matan Rosenberg <matan129@gmail.com>
2017-10-04 15:30:23 +0300

    Reformat test

--- src/test/java/org/elasticsearch/index/analysis/NetworkAddressAnalyzerTests.java
+++ src/test/java/org/elasticsearch/index/analysis/NetworkAddressAnalyzerTests.java
@@ -11,8 +11,7 @@ public class NetworkAddressAnalyzerTests extends BaseAnalyzerTest {
     }
 
     public void testNetworkAddressAnalyzerMac() throws Exception {
-        testAnalyzer(NetworkAddressAnalyzer.NAME, "22:4e:b2:53:d7:93 kn",
-                                     "22", "4e", "b2", "53", "d7", "93");
+        testAnalyzer(NetworkAddressAnalyzer.NAME, "22:4e:b2:53:d7:93 kn", "22", "4e", "b2", "53", "d7", "93");
     }
 
     public void testNetworkAddressAnalyzerMac2() throws Exception {
@@ -25,7 +24,8 @@ public class NetworkAddressAnalyzerTests extends BaseAnalyzerTest {
 
     public void testNetworkAddressAnalyzerMulti() throws Exception {
         testAnalyzerIgnoringPosition(NetworkAddressAnalyzer.NAME,
-                                     "aa 1.2.3.4 bb acw 22.44.33.42.1 22:4e:b2:53:d7:93 acDD.cccc.AAaa kn 224e:b253:d793",
+                                     "aa 1.2.3.4 bb acw 22.44.33.42.1 22:4e:b2:53:d7:93 acDD.cccc.AAaa kn " +
+                                     "224e:b253:d793",
                                      "1", "2", "3", "4", "22", "44", "33", "42", "22", "4e", "b2", "53", "d7", "93",
                                      "ac", "dd", "cc", "cc", "aa", "aa", "22", "4e", "b2", "53", "d7", "93");
     }
